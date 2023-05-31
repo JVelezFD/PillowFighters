1d2e21c2deb348eaa353a19758bbef083dfe12e4
JVelezFD <jvelez117@gmail.com>
2023-05-31 11:30:16 -0400

    initial project setup

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Pillow Fighters</title>
-</head>
-<body>
-    
-
-</body>
-</html>const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
 canvas.width = 1024
